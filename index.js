69698664e2dff40b6539325146b489da2a18c67a
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-10 20:33:18 +0800

    [FIX] Format

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-console.log("Hello world from Main Branch!")
+console.log("Hello world from Main Branch!");
 console.log("Hello world from Main Branch2!");
 console.log("Hello world from Dev Branch!");
 
-
 console.log("Unique message from Dev Branch!");
-console.log("Hello world from Main Branch3!")
-console.log("Hello world from Main Branch4!")
+console.log("Hello world from Main Branch3!");
+console.log("Hello world from Main Branch4!");
